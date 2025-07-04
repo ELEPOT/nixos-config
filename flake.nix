011e96bca059ea652ee03558be7905754935ae3c
ELEPOT <elepotmail0@gmail.com>
2025-07-04 08:42:11 +0800

    FIRST COMMIT

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Dont really matter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    swww.url = "github:LGFae/swww";
+#    niri.url = "github:sodiboo/niri-flake";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+  
+  in { 
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit system inputs; };
+        modules = [
+#          {
+#            nixpkgs.overlays = [
+#              inputs.niri.overlays.niri
+#            ];
+#          }
+          ./nixos/configuration.nix
+        ];
+      };
+    }; 
+  }
---
+++ nixos/configuration.nix
@@ -0,0 +1,181 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  users.users.elepot.packages = with pkgs; [
+    # cli tools
+    htop
+    wev # keytester
+    wget
+    git
+    rsync # better cp
+    ntfs3g # ntfs disk fixer
+    progress
+    pamixer
+    toybox
+    nmap
+
+    # tui
+    neovim
+  
+    # gui
+    inputs.zen-browser.packages.${system}.specific
+    obsidian
+    (blender.override { cudaSupport = true; })
+    alacritty # terminal
+    vesktop # discord client
+    swayimg
+    bottles
+
+    # desktop
+    eww
+    wl-clipboard
+    labwc
+    xwayland-satellite
+    fuzzel # app launcher
+    keymapper
+    dunst # notification handler
+    inputs.swww.packages.${system}.swww # bg manager
+    xdg-desktop-portal
+    kdePackages.xdg-desktop-portal-kde
+    xsettingsd
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Taipei";
+
+  i18n.defaultLocale = "zh_TW.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-anthy
+      catppuccin-fcitx5
+    ];
+    fcitx5.waylandFrontend = true;
+  };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+  ];
+
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.xserver.xkb = {
+    layout = "tw";
+    variant = "";
+  };
+
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother_HL-1210W";
+        deviceUri = "http://192.168.1.170";
+        model = "brother-HL1210W-cups-en.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+
+    ensureDefaultPrinter = "Brother_HL-1210W";
+  };
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.cups-brother-hl1210w ];
+  };
+  
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  systemd.services.keymapperd = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = ''${pkgs.keymapper}/bin/keymapperd'';
+    };
+  };
+
+  users.users.elepot = {
+    isNormalUser = true;
+    description = "ELEPOT";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  programs.niri.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  environment.variables = {
+    DISPLAY = ":0";
+    EDITOR = "nvim";
+    SYSTEMD_EDITOR = "nvim";
+    VISUAL = "nvim";
+    GTK_IM_MODULE = "";
+    QT_IM_MODULE = "";
+    XMODIFIERS = "@im=fcitx";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+  };
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
