dcce57f439ebe5f66fd4a286d7d686700dfe36d8
ELEPOT <elepotmail0@gmail.com>
2025-07-06 22:28:28 +0800

    166

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,8 +105,8 @@
   services.xserver.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
     layout = "tw";
