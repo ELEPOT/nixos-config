4ea31808c4ae9fa4e25f8d82532679ac441552f8
ELEPOT <elepotmail0@gmail.com>
2025-07-14 13:40:57 +0800

    224

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,6 +144,17 @@
                 <string>NotoSansTC</string>
             </edit>
           </match>
+          <match target="pattern">
+            <test name="lang" compare="contains">
+                <string>zh</string>
+            </test>
+            <test qual="any" name="family">
+                <string>mono</string>
+            </test>
+            <edit name="family" mode="prepend" binding="strong">
+                <string>NotoSansTC</string>
+            </edit>
+          </match>
         </fontconfig>
       '';
     };
