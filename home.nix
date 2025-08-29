4de6b4805ceabe4b69459b034536e8b07e4c48a5
ELEPOT <elepotmail0@gmail.com>
2025-08-29 14:52:51 +0800

    25

--- home.nix
+++ home.nix
@@ -95,9 +95,11 @@
       '';
     };
 
-    "./.local/share/gnome-shell/extensions/switch-keyboard-layout@maksmartyn" = {
-      source = "${self}/gnome-switch-keyboard-layout";
-      recursive = true;
+    "./.local/share/gnome-shell/extensions/switch-keyboard-layout@maksmartyn/extension.js" = {
+      source = "${self}/gnome-switch-keyboard-layout/extension.js";
+    };
+    "./.local/share/gnome-shell/extensions/switch-keyboard-layout@maksmartyn/metadata.json" = {
+      source = "${self}/gnome-switch-keyboard-layout/metadata.json";
     };
   };
 
