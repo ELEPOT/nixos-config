4d4fabdab551045a1abb940fcf1883f79d543f74
ELEPOT <elepotmail0@gmail.com>
2025-09-07 20:22:39 +0800

    27

--- home.nix
+++ home.nix
@@ -120,8 +120,23 @@
         "application/x-codeblocks"
         "application/x-codeblocks-workspace"
       ];
-      settings = {
-        X-MultipleArgs = "false";
+    };
+
+    "org.gnome.Nautilus" = {
+      name = "nautilus";
+      genericName = "n";
+      exec = "nautilus --new-window %U";
+      icon = "org.gnome.Nautilus";
+      terminal = false;
+      type = "Application";
+      categories = ["GNOME" "GTK" "Utility" "Core" "FileManager"];
+      startupNotify = false;
+      mimeType = ["inode/directory" "application/x-7z-compressed" "application/x-7z-compressed-tar" "application/x-bzip" "application/x-bzip-compressed-tar" "application/x-compress" "application/x-compressed-tar" "application/x-cpio" "application/x-gzip" "application/x-lha" "application/x-lzip" "application/x-lzip-compressed-tar" "application/x-lzma" "application/x-lzma-compressed-tar" "application/x-tar" "application/x-tarz" "application/x-xar" "application/x-xz" "application/x-xz-compressed-tar" "application/zip" "application/gzip" "application/bzip2" "application/x-bzip2-compressed-tar" "application/vnd.rar" "application/zstd" "application/x-zstd-compressed-tar"];
+      actions = {
+        "new-window" = {
+          name = "New Window";
+          exec = "nautilus --new-window";
+        };
       };
     };
   };
