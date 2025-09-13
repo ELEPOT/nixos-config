c4cedad7ed2d425ed06db7c87c58914c97470531
ELEPOT <elepotmail0@gmail.com>
2025-09-13 22:49:21 +0800

    hello

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,7 @@
   functions,
   ...
 }: {
-  imports = [./device_specific/configuration.nix];
+  imports = [] ++ functions.ifExists ../device_specific/configuration.nix;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
