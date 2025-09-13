5868542d7e5f05a335824523a6645f5867ce37af
ELEPOT <elepotmail0@gmail.com>
2025-09-13 23:58:29 +0800

    aaah

---
+++ device_specific/home.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  pkgs,
+  inputs,
+  functions,
+  ...
+}: {
+  home.file = {
+    "./.config/keymapper.conf" = {
+      text = ''
+        Alt{Space{Any}} >> Alt{Any}
+        Alt{Space{Shift{Any}}} >> Alt{Shift{Any}}
+        Control{Alt{Space{Any}}} >> Control{Alt{Any}}
+        Control{Shift{Alt{Space{Any}}}} >> Control{Shift{Alt{Any}}}
+
+        Alt{Space} >> Alt{Space}
+        Alt{Shift{Space}} >> Alt{Shift{Space}}
+
+        PageDown >> $(sh -c "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+") ^
+        PageUp   >> $(sh -c "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-") ^
+      '';
+    };
+
+    "./.profile" = {
+      text = ''
+        keymapper -u &
+      '';
+    };
+  };
+}
