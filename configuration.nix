f2629755d2ea3a14003951c6848a6c9c0ae96095
ELEPOT <elepotmail0@gmail.com>
2026-04-25 20:18:16 +0800

    237

--- configuration.nix
+++ configuration.nix
@@ -307,13 +307,13 @@
     reaper
     vital
     godot
-    pkgs.libreoffice-unwrapped.override
-    {
-      python3 = pkgs.python312.withPackages (python-pkgs: [
-        python-pkgs.pygments
-        python-pkgs.catppuccin
-      ]);
-    }
+    (pkgs.libreoffice-unwrapped.override
+      {
+        python3 = pkgs.python312.withPackages (python-pkgs: [
+          python-pkgs.pygments
+          python-pkgs.catppuccin
+        ]);
+      })
 
     keymapper
     xdg-desktop-portal
