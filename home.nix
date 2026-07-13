54291fac3a288ec9573ca36039400be311f6223c
ELEPOT <elepotmail0@gmail.com>
2026-07-13 10:53:47 +0800

    45

--- home.nix
+++ home.nix
@@ -202,22 +202,13 @@
       };
 
       cmp = {
+        enable = true;
         autoEnableSources = true;
         settings.sources = [
           {name = "nvim_lsp";}
           {name = "path";}
           {name = "buffer";}
         ];
-      };
-
-      cmp-nvim-lsp = {
-        enable = true; # LSP
-      };
-      cmp-buffer = {
-        enable = true;
-      };
-      cmp-path = {
-        enable = true; # file system paths
       };
     };
 
